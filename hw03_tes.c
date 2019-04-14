01cf0390bf0dbeacb7a88af78224f567f0a9386e
Lukas Hajda <xhajda@fi.muni.cz>
2019-04-14 00:38:29 +0000

    all sub-functions are done (maybe)

--- hw03_tes.c
+++ hw03_tes.c
@@ -139,7 +139,7 @@ int osc_message_add_int32(struct osc_message *msg, int32_t data) {
     return 1;
   }
   size_t newTtagLen = getLength(newTypeTag);
-  void *pData = realloc(msg->raw_data, (newTtagLen + adrLen + 8));
+  void *pData = realloc(msg->raw_data, (newTtagLen + adrLen + sizeof(int) + sizeof(int32_t)));
 
   if (!pData) {
     msg->raw_data = oldPtr;
@@ -157,8 +157,9 @@ int osc_message_add_int32(struct osc_message *msg, int32_t data) {
   *addDataPointer = data;
   
   int *pRawData = pData;
-  *pRawData = (newTtagLen + adrLen + 4);
+  *pRawData = (newTtagLen + adrLen + sizeof(data));
   msg->raw_data = pRawData;
+
   // Tu sa vykonava free.
   free(newTypeTag);
 
@@ -166,9 +167,121 @@ int osc_message_add_int32(struct osc_message *msg, int32_t data) {
 
 }
 
-int main(void) {
+int osc_message_add_float(struct osc_message *msg, float data) {
+  size_t adrLen = getLength(msg->address);
+  // TOTO FREE
+  void *oldPtr = msg->raw_data;
+  char *newTypeTag = addNewTypeTag(msg->typetag, OSC_TT_FLOAT);
+
+  if (!newTypeTag) {
+    msg->raw_data = oldPtr;
+    return 1;
+  }
+  size_t newTtagLen = getLength(newTypeTag);
+  void *pData = realloc(msg->raw_data, (newTtagLen + adrLen + 8));
+
+  if (!pData) {
+    msg->raw_data = oldPtr;
+    return 1;
+  }
+
+  char *pAddress = pData + 4;
+  msg->address = pAddress;
+
+  char *pTypeTag = pData + 4 + adrLen;
+  memcpy(pTypeTag, newTypeTag, newTtagLen);
+  msg->typetag = pTypeTag;
+
+  float *addDataPointer = pData + 4 + adrLen + newTtagLen;
+  *addDataPointer = data;
+  
+  int *pRawData = pData;
+  *pRawData = (newTtagLen + adrLen + sizeof(data));
+  msg->raw_data = pRawData;
+
+  // Tu sa vykonava free.
+  free(newTypeTag);
+
   return 0;
 }
 
+int osc_message_add_timetag(struct osc_message *msg, struct osc_timetag tag) {
+  size_t adrLen = getLength(msg->address);
+  // TOTO FREE
+  void *oldPtr = msg->raw_data;
+  char *newTypeTag = addNewTypeTag(msg->typetag, OSC_TT_TIMETAG);
 
+  if (!newTypeTag) {
+    msg->raw_data = oldPtr;
+    return 1;
+  }
+  size_t newTtagLen = getLength(newTypeTag);
+  void *pData = realloc(msg->raw_data, (newTtagLen + adrLen + sizeof(int) + sizeof(struct osc_timetag)));
 
+  if (!pData) {
+    msg->raw_data = oldPtr;
+    return 1;
+  }
+
+  char *pAddress = pData + 4;
+  msg->address = pAddress;
+
+  char *pTypeTag = pData + 4 + adrLen;
+  memcpy(pTypeTag, newTypeTag, newTtagLen);
+  msg->typetag = pTypeTag;
+  //mozno picovina
+  struct osc_timetag *addTagPointer = pData + 4 + adrLen + newTtagLen;
+  *addTagPointer = tag;
+  
+  int *pRawData = pData;
+  *pRawData = (newTtagLen + adrLen + sizeof(tag));
+  msg->raw_data = pRawData;
+
+  // Tu sa vykonava free.
+  free(newTypeTag);
+
+  return 0;
+}
+
+int osc_message_add_string(struct osc_message *msg, const char *data) {
+  size_t adrLen = getLength(msg->address);
+  // TOTO FREE
+  void *oldPtr = msg->raw_data;
+  char *newTypeTag = addNewTypeTag(msg->typetag, OSC_TT_STRING);
+
+  if (!newTypeTag) {
+    msg->raw_data = oldPtr;
+    return 1;
+  }
+  size_t newTtagLen = getLength(newTypeTag);
+  // +1 for ending zero
+  void *pData = realloc(msg->raw_data, (newTtagLen + adrLen + sizeof(int) + (strlen(data) + 1)*sizeof(char)));
+
+  if (!pData) {
+    msg->raw_data = oldPtr;
+    return 1;
+  }
+
+  char *pAddress = pData + 4;
+  msg->address = pAddress;
+
+  char *pTypeTag = pData + 4 + adrLen;
+  memcpy(pTypeTag, newTypeTag, newTtagLen);
+  msg->typetag = pTypeTag;
+  //mozno picovina
+  char *addDataPointer = pData + 4 + adrLen + newTtagLen;
+  memcpy(addDataPointer, data, strlen(data) + 1);
+  
+  int *pRawData = pData;
+  *pRawData = (newTtagLen + adrLen + 4);
+  msg->raw_data = pRawData;
+
+  // Tu sa vykonava free.
+  free(newTypeTag);
+
+  return 0;
+}
+
+int main(void) {
+  return 0;
+}
